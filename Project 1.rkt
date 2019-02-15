8c4f86b5fb743d4a34fbdb00018acbcd83e517e2
markgoldberg <mark.goldberg@gmail.com>
2019-02-15 16:44:22 -0500

    made file, put in helpful in class example

---
+++ Project 1.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+
+
+
+;;this should help
+(define mvalue3
+  (lambda (lis)
+    (cond
+      [(number? lis) lis]
+      [(eq? (operator lis) '+) (+ (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
+      [(eq? (operator lis) '-) (- (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
+      [(eq? (operator lis) '*) (* (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
+      [(eq? (operator lis) '/) (quotient (mvalue3 (operand1 lis)) (mvalue3 operand2 lis))]
+      [(eq? (operator lis) '%) (remainder (mvalue3 (operand1 lis)) (mvalue3 operand2 lis))])))
