aad61b182e492e8286529e4b9b70f99b19492cec
markgoldberg <mark.goldberg@gmail.com>
2019-02-15 17:10:47 -0500

    included mvalue and got parser working

--- Project 1.rkt
+++ Project 1.rkt
@@ -1,21 +1,39 @@
 #lang racket
-require "simpleParser.rkt"
+(require "simpleParser.rkt")
 
+;;where it starts
 (define main
+  (lambda (filename)
+    (m (parser filename))))
+
+;;recurses after gets pared file
+(define m
   (lambda (lis)
     (cond
-
+      [(isincluded (car lis) '(+ - * / %)) (mvalue lis)]
       )))
 
 
-
-;;this should help
-(define mvalue3
+;;mvalue for math
+(define mvalue
   (lambda (lis)
     (cond
       [(number? lis) lis]
-      [(eq? (operator lis) '+) (+ (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
-      [(eq? (operator lis) '-) (- (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
-      [(eq? (operator lis) '*) (* (mvalue3 (operand1 lis)) (mvalue3 (operand2 lis)))]
-      [(eq? (operator lis) '/) (quotient (mvalue3 (operand1 lis)) (mvalue3 operand2 lis))]
-      [(eq? (operator lis) '%) (remainder (mvalue3 (operand1 lis)) (mvalue3 operand2 lis))])))
+      [(eq? (operator lis) '+) (+ (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
+      [(eq? (operator lis) '-) (- (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
+      [(eq? (operator lis) '*) (* (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
+      [(eq? (operator lis) '/) (quotient (mvalue (operand1 lis)) (mvalue operand2 lis))]
+      [(eq? (operator lis) '%) (remainder (mvalue (operand1 lis)) (mvalue operand2 lis))])))
+
+(define operator car)
+(define operand1 cadr)
+(define operand2 caddr)
+
+
+;;checks if atom is the same as any atom in a list (not * for a reason)
+(define isincluded
+  (lambda (a lis)
+    (cond
+      [(null? lis) #f]
+      [(eq? (car lis) a) #t]
+      [else (isincluded a (cdr lis))])))
