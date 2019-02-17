6ac2b931235d142b119b4986ed20ea46fff18150
markgoldberg <mark.goldberg@gmail.com>
2019-02-17 17:04:41 -0500

    got everything except return working (though the reason may be in mstate)

--- Project 1.rkt
+++ Project 1.rkt
@@ -4,32 +4,49 @@
 ;;where it starts
 (define main
   (lambda (filename)
-    (m (parser filename))))
+    (mstate (parser filename) '())))
 
-;;recurses after gets pared file
-(define m
-  (lambda (lis)
-    (cond
-      [(isincluded (car lis) '(+ - * / %)) (mvalue lis)]
-      [(isincluded (car lis) '(> >= < <= ==)) (mbool lis)]
-      )))
 
+;;
 (define mstate
-  (lambda (lis)
-    5))
+  (lambda (lis state)
+    (cond
+      [(null? lis) (return state)]
+      [(number? (car lis)) (car lis)]
+      [(boolean? (car lis)) (car lis)]
+      [(eq? (caar lis) 'return) (mstate (cdr lis) (addreturn state (cdr lis)))]
+      [(eq? (caar lis) 'var) (mstate (cdr lis) (instantiatevar (car lis) state))]
+      [(eq? (caar lis) '=) (mstate (cdr lis) (updatevar (car lis) state))]
+      [(eq? (caar lis) 'if) (mstate (cdr lis) (mif (car lis) state))]
+      [(isincluded (caar lis) '(+ - * / %)) (mvalue (car lis))]
+      [(isincluded (caar lis) '(> >= < <= ==)) (mbool (car lis))])))
 
+
+;;gets return val
+(define return
+  (lambda (state)
+    (cond
+      [(null? state) 'error]
+      [(eq? (caar state) 'return) (car (cdr (car state)))]
+      [else (return (cdr state))])))
+
+(define addreturn
+  (lambda (state val)
+    (append state (list (cons 'return (mstate val state))))))
+    
 
 
 ;;mvalue for math
 (define mvalue
-  (lambda (lis)
+  (lambda (lis state)
     (cond
       [(number? lis) lis]
-      [(eq? (operator lis) '+) (+ (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '-) (- (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '*) (* (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '/) (quotient (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '%) (remainder (mvalue (operand1 lis)) (mvalue (operand2 lis)))])))
+      [(not (list? lis)) (lookup lis state)]
+      [(eq? (operator lis) '+) (+ (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '-) (- (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '*) (* (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '/) (quotient (mvalue (operand1 lis)) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '%) (remainder (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))])))
 
 (define operator car)
 (define operand1 cadr)
@@ -39,27 +56,29 @@
 
 ;;mbool for boolean logic
 (define mbool
-  (lambda (lis)
+  (lambda (lis state)
     (cond
-      [(or (eq? lis #t) (eq? lis #f)) lis]
-      [(eq? (operator lis) '>) (> (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '>=) (>= (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '<) (mbool '(!(> 5 (5 + 5))))(< (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '<=) (<= (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '==) (eq? (mvalue (operand1 lis)) (mvalue (operand2 lis)))]
-      [(eq? (operator lis) '!=) (not (eq? (mvalue (operand1 lis)) (mvalue (operand2 lis))))]
-      [(eq? (operator lis) '&&) (and (mbool (operand1 lis)) (mbool (operand2 lis)))]
-      [(eq? (operator lis) '||) (or (mbool (operand1 lis)) (mbool (operand2 lis)))]
-      [(eq? (operator lis) '!) (not (mbool (operand1 lis)))])))
+      [(boolean? lis) lis]
+      [(not (list? lis)) (lookup lis state)]
+      [(eq? (operator lis) '>) (> (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '>=) (>= (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '<) (< (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '<=) (<= (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '==) (eq? (mvalue (operand1 lis) state) (mvalue (operand2 lis) state))]
+      [(eq? (operator lis) '!=) (not (eq? (mvalue (operand1 lis) state) (mvalue (operand2 lis) state)))]
+      [(eq? (operator lis) '&&) (and (mbool (operand1 lis) state) (mbool (operand2 lis) state))]
+      [(eq? (operator lis) '||) (or (mbool (operand1 lis) state) (mbool (operand2 lis) state))]
+      [(eq? (operator lis) '!) (not (mbool (operand1 lis) state))])))
+
 
 
 
 ;;for if statements
 (define mif
-  (lambda (lis)
+  (lambda (lis state)
     (cond
-      [(mbool (cadr lis)) (caddr lis)]
-      [(hasNestedIf lis) (mif (cadddr lis))])))
+      [(mbool (cadr lis) state) (mstate (list (caddr lis)) state)]
+      [(hasNestedIf lis) (mif (cadddr lis) state)])))
 
 ;;checks if there's and else if
 (define hasNestedIf
@@ -82,9 +101,39 @@
 
 ;;implementation of while loops      ******untested (needs vars)
 (define mwhile
-  (lambda (lis)
+  (lambda (lis state)
     (cond
-      [(mbool (cadr lis)) (mwhile (mstate (caddr lis)))])))
+      [(mbool (cadr lis)) (mwhile (mstate (caddr lis) state) '())])))
+
+
+
+;;instatiate variables
+(define instantiatevar
+  (lambda (lis state)
+    (cons (list (cadr lis)) state)))
+
+
+;;default use of updatevar
+(define updatevar
+  (lambda (lis state)
+    (updatevar-acc lis state '())))
+
+;;set variable
+(define updatevar-acc
+  (lambda (lis state acc)
+    (cond
+      [(null? state) acc]
+      [(eq? (caar state) (cadr lis)) (updatevar-acc lis (cdr state) (append acc (list (cons (cadr lis) (list (caddr lis))))))]
+      [else (updatevar-acc lis (cdr state) (append acc (list (car state))))])))
+
+
+;;finds saved value of var
+(define lookup
+  (lambda (var state)
+    (cond
+      [(null? state) 'error]
+      [(eq? (caar state) var) (cadar state)]
+      [else (lookup var (cdr state))])))
 
 
 
